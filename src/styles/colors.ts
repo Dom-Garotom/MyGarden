061861bcc7e83d89f804ca289963cd6b0e4e055c
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-11-04 06:31:26 -0300

    setup do projeto

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+export const color = {
+    green: {
+      bg: '#E6ECDE',
+      100: '#F73333',
+      200: '#C3DEA9',
+      300: '#00CD8F',
+      400: '#5FC6C3',
+      500: '#234723',
+    },
+    utils: {
+      white: '#FFFFFF',
+      black: '#090C0D',
+      subText: '#A7B9C7',
+      danger: '#F73333',
+    },
+  };
+  
