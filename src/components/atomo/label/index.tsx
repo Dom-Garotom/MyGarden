db405c71752d223903fb9e16af16e296e20be182
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-11-06 10:52:31 -0300

    feat: criando label que guarda task e status da task

---
+++ src/components/atomo/label/index.tsx
@@ -0,0 +1,30 @@
+import {  View , Text} from "react-native";
+import { styles } from "./styles";
+
+type Props = {
+  text: string
+  variant: "task" | "pendente" | "conclude"
+}
+
+export default function Label({ text, variant }: Props) {
+
+  const getVariant = () =>{
+    switch(variant){
+      case "pendente":
+         return styles.pendente;
+      case "conclude":
+        return styles.conclude;
+      case "task" :
+        return styles.task;
+      default: 
+        return {};
+    }
+  }
+
+  return (
+    <View style={[getVariant()]}>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  )
+}
+
