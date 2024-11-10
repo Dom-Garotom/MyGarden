18d3fd54eb5fd644e141579499e159145e5ababe
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-11-10 10:18:32 -0300

    feat: adicionando a capacidade de concluir e remover uma task

--- src/components/molecula/modalTaskItem/index.tsx
+++ src/components/molecula/modalTaskItem/index.tsx
@@ -1,10 +1,11 @@
-import { Alert, Linking, Modal, Text, TouchableOpacity, View } from "react-native";
+import { Modal, Text, TouchableOpacity, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useState } from "react";
 import { color } from "@/src/styles/colors";
 import Option from "../../atomo/option";
 import Label from "../../atomo/label";
 import { styles } from "./style";
+import { getTask, key_task_storage, saveToStorage, Task } from "@/src/storage/task-storage";
 
 type Props = {
     id: string
@@ -18,6 +19,48 @@ type Props = {
 export default function ModalTaskItem({ category, name, specie, id  , status = "pendente"}: Props) {
     const [close, setClose] = useState(true)
 
+    const concludeTask = async () =>{
+        const Tasks : Task[] = await getTask();
+        const CategoryIndex = Tasks.findIndex( item => item.categoryName === category)
+        
+        if (CategoryIndex == -1){
+            return
+        }
+        
+        const habitsIndex = Tasks[CategoryIndex].habits.findIndex(item => item.plantName === name);
+        
+        if (habitsIndex == -1){
+            return
+        }
+        
+        Tasks[CategoryIndex].habits[habitsIndex].status = "conclude"
+
+        saveToStorage(key_task_storage , Tasks )
+
+        setClose(false)
+    }
+
+    const removeTask = async () =>{
+        const Tasks : Task[] = await getTask();
+        const remove =  Tasks.map( item => {
+            if ( item.categoryName === category ){
+                const itensAptos = item.habits.filter( habit => habit.plantName !== name && habit.species === specie);
+
+                return {
+                    ...item,
+                    habits: itensAptos,
+                };
+            }
+
+            return item;
+        }).filter(item => item.habits.length > 0);
+
+        saveToStorage(key_task_storage , remove )
+
+        setClose(false)
+    }
+
+
     return (
         <Modal transparent visible={close} animationType="slide">
             <View style={styles.modal}>
@@ -27,7 +70,7 @@ export default function ModalTaskItem({ category, name, specie, id  , status = "
 
                         <Text style={styles.modalCategory}>{category}</Text>
 
-                        <TouchableOpacity onPress={() => setClose(!close)}>
+                        <TouchableOpacity onPress={() => setClose(false)}>
                             <MaterialIcons name="exit-to-app" size={20} color={color.utils.black} />
                         </TouchableOpacity>
 
@@ -45,8 +88,8 @@ export default function ModalTaskItem({ category, name, specie, id  , status = "
                     </View>
 
                     <View style={styles.optionContainer}>
-                        <Option name="Excluir" icon="delete-sweep" variant="secundary"  />
-                        <Option name="Concluir" icon="done-all" />
+                        <Option name="Excluir" icon="delete-sweep" variant="secundary"  onPress={() => removeTask()} />
+                        <Option name="Concluir" icon="done-all" onPress={() => concludeTask()} />
                     </View>
                 </View>
 
